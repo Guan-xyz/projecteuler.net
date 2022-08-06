803ce359db9392237e17b064003cc8092e7b791a
Pizza Nick‘s <pizzanicks@Pizzas-MacBook-Pro.local>
2022-08-06 19:54:52 +0800

    10001st prime

---
+++ pe_7_10001st.py
@@ -0,0 +1,46 @@
+"""
+NicksPizza
+Project Euler .net
+
+10001st prime
+Problem 7
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+Answer: 104743
+"""
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+    elif n < 4:
+        return True
+    elif n % 2 == 0:
+        return False
+    elif n < 9:
+        return True
+    elif n % 3 == 0:
+        return False
+    r = int(n ** 0.5)
+    f = 5
+    while f <= r:
+        if n % f == 0:
+            return False
+        if n % (f+2) == 0:
+            return False
+        f = f + 6
+    return True
+
+
+count = 1
+results = 1
+while count < 10001:
+    results = results + 2
+    if is_prime(results):
+        count += 1
+print(results)
+
+
