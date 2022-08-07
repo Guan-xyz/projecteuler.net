89c358748748490db22167122f94ed801838ef9f
Pizza Nick‘s <pizzanicks@Pizzas-MacBook-Pro.local>
2022-08-07 21:50:45 +0800

    ?

---
+++ pe_8_largest.py
@@ -0,0 +1,18 @@
+"""
+NicksPizza
+Project Euler .net
+
+Largest product in a series
+Problem 8
+
+The four adjacent digits in the 1000-digit number
+that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+
+Find the thirteen adjacent digits in the 1000-digit number that have the greatest product.
+What is the value of this product?
+
+Answer:
+"""
+
+
+result = '?'
